b5d41bbc83a18b496f7e499a16fa8634a79721b6
Fatima Azzahro <azzahro.fatima@gmail.com>
2020-12-22 18:23:27 +0700

    ATM - Create looping Main Menu

--- atm_program.py
+++ atm_program.py
@@ -1,11 +1,24 @@
 from atm_card import AtmCard
 from customer import Customer
 
-main_menus = ['Exit', 'Create an Account', 'Log into an Account']
-index_main_menu = 0
+import random
 
-for main_menu in main_menus:
-    print(str(index_main_menu) + '. ' + main_menu)
-    index_main_menu += 1
+while (True):
 
-print('--------------------')
+    main_menus = ['Exit', 'Create an Account', 'Log into an Account']
+    index_main_menu = 0
+
+    for main_menu in main_menus:
+        print(str(index_main_menu) + '. ' + main_menu)
+        index_main_menu += 1
+
+    print('--------------------')
+
+    main_menu = int(input('Masukkan nomer menu: '))
+
+    if (main_menu == 1):
+        print('Your card has been created')
+    elif (main_menu == 2):
+        print('Ok')
+    else:
+        exit()
