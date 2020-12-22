c6ff9b15aa7833f052f6669ac0a76c6c78ea8556
Fatima Azzahro <azzahro.fatima@gmail.com>
2020-12-22 20:44:08 +0700

    Customer - Apply inheritance from ATM Card

--- customer.py
+++ customer.py
@@ -1,20 +1,12 @@
 from atm_card import AtmCard
 
 
-class Customer:
-    def __init__(self, id, custPin=1234, custBalance=10000):
-        self.id = id
-        self.custPin = custPin
-        self.custBalance = custBalance
-
-    def checkId(self):
-        return self.id
-
-    def checkPin(self):
-        return self.custPin
-
-    def checkBalance(self):
-        return self.custBalance
+class Customer(AtmCard):
+    def __init__(self,
+                 custId=4000000000000000,
+                 custPin=1234,
+                 custBalance=10000):
+        super().__init__(custId, custPin, custBalance)
 
     def debet(self, nominal):
         self.custBalance -= nominal
