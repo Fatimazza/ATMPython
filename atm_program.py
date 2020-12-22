dd2137a8ea2f65b5e8f06c62d71884df597251aa
Fatima Azzahro <azzahro.fatima@gmail.com>
2020-12-22 16:26:57 +0700

    ATM - Test new Customer, Test Debet and Credit

--- atm_program.py
+++ atm_program.py
@@ -1,2 +1,10 @@
-print('Hello World')
-print(1+2)
+from atm_card import AtmCard
+from customer import Customer
+
+customer1 = Customer(2222)
+print(customer1.checkPin())
+print(customer1.checkBalance())
+print(customer1.credit(600))
+print(customer1.checkBalance())
+print(customer1.debet(500))
+print(customer1.checkBalance())
